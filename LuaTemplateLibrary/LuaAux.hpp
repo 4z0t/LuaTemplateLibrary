03b3a8e216dd4ed26353da60987e630304bd1ef8
4z0t <gastec139@gmail.com>
2023-10-15 13:42:54 +0300

    cleanup

--- LuaTemplateLibrary/LuaAux.hpp
+++ LuaTemplateLibrary/LuaAux.hpp
@@ -55,14 +55,13 @@ namespace Lua
     template<typename RType = void, typename ...Ts>
     RType CallFunction(lua_State* l, const char* name, Ts&&... args)
     {
+        size_t n = _PrepareCall(l, name, std::forward<Ts>(args)...);
         if constexpr (std::is_void_v<RType>)
         {
-            size_t n = _PrepareCall(l, name, std::forward<Ts>(args)...);
             lua_call(l, n, 0);
         }
         else
         {
-            size_t n = _PrepareCall(l, name, std::forward<Ts>(args)...);
             lua_call(l, n, 1);
             RType result = TypeParser<RType>::Get(l, 1);
             lua_pop(l, 1);
