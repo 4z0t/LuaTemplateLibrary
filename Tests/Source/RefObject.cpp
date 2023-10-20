cd22cbdd811a9fea16e2e81dcda906c6aebf9c72
4z0t <gastec139@gmail.com>
2023-10-21 01:28:18 +0300

    add test for operator

--- Tests/Source/RefObject.cpp
+++ Tests/Source/RefObject.cpp
@@ -11,6 +11,7 @@ TEST_F(RefObjectTests, ValueAccess)
     Run("result = true");
     ASSERT_TRUE(Result().Is<bool>());
     ASSERT_TRUE(Result().To<bool>());
+    ASSERT_TRUE((bool)Result());
    
 
 
