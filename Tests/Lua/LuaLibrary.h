ab874ff474bf10d860434a019e1ffef7488dbd99
Alexander <84857900+4z0t@users.noreply.github.com>
2023-10-30 11:03:06 +0300

    convert to submodule

--- Tests/Lua/LuaLibrary.h
+++ Tests/Lua/LuaLibrary.h
@@ -1,17 +1,12 @@
 #pragma once
 
-#if LUA_VERSION <= 501
+#if LUA_VERSION >= 501
 
 extern "C" {
 #include "lauxlib.h"
 #include "lua.h"
 #include "lualib.h"
 }
-
-#elif LUA_VERSION <= 504
-
-#include "lua.hpp"
-
 #else
 
 #error "LUA_VERSION is not defined"
