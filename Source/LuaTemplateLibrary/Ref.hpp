c1793d8947086d8d77dedd4eb1e0f6d9c26d6fee
4z0t <gastec139@gmail.com>
2024-04-15 00:47:25 +0300

    add briefs

--- Source/LuaTemplateLibrary/Ref.hpp
+++ Source/LuaTemplateLibrary/Ref.hpp
@@ -4,10 +4,16 @@
 
 namespace LTL
 {
-    template<typename RefAccess = RefGlobalAccess>
+    template <typename RefAccess = RefGlobalAccess>
     class Ref;
 
-    template<typename RefAccess = RefGlobalAccess>
+    /**
+     * @brief Класс контекста для Ref. Совмещает в себе состояние ВМ и ссылку на объект.
+     * Уничтожение объекта должно быть выполнено вручную.
+     * @see Ref
+     * @tparam RefAccess
+     */
+    template <typename RefAccess = RefGlobalAccess>
     class ContextRef
     {
     public:
@@ -15,27 +21,25 @@ namespace LTL
 
         ContextRef() = delete;
 
-        ContextRef(lua_State* l, const RefClass& ref) : m_state{ l }, m_ref{ ref } {}
+        ContextRef(lua_State *l, const RefClass &ref) : m_state{l}, m_ref{ref} {}
 
-        ContextRef(const ContextRef& other) :m_state{ other.m_state }, m_ref{ other.m_ref } {}
+        ContextRef(const ContextRef &other) : m_state{other.m_state}, m_ref{other.m_ref} {}
 
-        ContextRef(ContextRef&& other) : m_state{ other.m_state }, m_ref{ std::move(other.m_ref) } {}
+        ContextRef(ContextRef &&other) : m_state{other.m_state}, m_ref{std::move(other.m_ref)} {}
 
-        ContextRef& operator=(const ContextRef& other)
+        ContextRef &operator=(const ContextRef &other)
         {
             m_state = other.m_state;
             m_ref = other.m_ref;
             return *this;
         }
 
-        ContextRef& operator=(ContextRef&& other)
+        ContextRef &operator=(ContextRef &&other)
         {
             m_state = std::move(other.m_state);
             m_ref = std::move(other.m_ref);
             return *this;
         }
-
-
 
         void Release()
         {
@@ -43,77 +47,85 @@ namespace LTL
         }
 
         ~ContextRef() = default;
+
     private:
         friend class RefClass;
-        lua_State* m_state = nullptr;
+        lua_State *m_state = nullptr;
         RefClass m_ref{};
     };
 
-    template<typename RefAccess = RefGlobalAccess>
+    /**
+     * @brief Класс ссылки на объект внутри ВМ. представляет из себя только ссылку на объект без самой ВМ.
+     * Уничтожение объекта должно быть выполнено вручную. Имеется класс-обертка для работы со ссылаемым объектом.
+     * @see ContextRef
+     * @tparam RefAccess
+     */
+    template <typename RefAccess = RefGlobalAccess>
     class Ref
     {
     public:
         Ref() {}
 
-        Ref(lua_State* l, int index)
+        Ref(lua_State *l, int index)
         {
             lua_pushvalue(l, index);
             _FromTop(l);
         }
 
-        Ref(const ContextRef<RefAccess>& sref) : Ref(sref.m_ref) {}
+        Ref(const ContextRef<RefAccess> &sref) : Ref(sref.m_ref) {}
 
-        Ref(const Ref& other) noexcept : m_ref{ other.m_ref } {}
+        Ref(const Ref &other) noexcept : m_ref{other.m_ref} {}
 
-        Ref(Ref&& other) noexcept : m_ref{ other.m_ref }
+        Ref(Ref &&other) noexcept : m_ref{other.m_ref}
         {
             other.NullRef();
         }
 
-        Ref& operator=(const ContextRef<RefAccess>& sref)
+        Ref &operator=(const ContextRef<RefAccess> &sref)
         {
             *this = sref.m_ref;
             return *this;
         }
 
-        Ref& operator=(const Ref& other)
+        Ref &operator=(const Ref &other)
         {
             m_ref = other.m_ref;
             return *this;
         }
 
-        Ref& operator=(Ref&& ref) noexcept
+        Ref &operator=(Ref &&ref) noexcept
         {
             m_ref = ref.m_ref;
             ref.NullRef();
             return *this;
         }
 
-        void Push(lua_State* l)const
+        void Push(lua_State *l) const
         {
             RefAccess::PushRef(l, ref);
         }
 
-        void Unref(lua_State* l)
+        void Unref(lua_State *l)
         {
             RefAccess::Unref(l, ref);
             NullRef();
         }
 
-        void Duplicate(lua_State* l, const Ref& other)
+        void Duplicate(lua_State *l, const Ref &other)
         {
             _FromRef(l, other);
         }
 
         ~Ref() = default;
+
     private:
-        void _FromRef(lua_State* l, const Ref& other)
+        void _FromRef(lua_State *l, const Ref &other)
         {
             other.Push();
             _FromTop(l);
         }
 
-        void _FromTop(lua_State* l)
+        void _FromTop(lua_State *l)
         {
             m_ref = RefAccess::GetRef(l);
         }
