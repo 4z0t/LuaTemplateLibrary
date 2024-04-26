fefc7c5bf9d5004e51efdcd37c8d412294f5c71b
4z0t <gastec139@gmail.com>
2024-04-26 19:29:40 +0300

    Update Libs.hpp

--- Source/LuaTemplateLibrary/Libs.hpp
+++ Source/LuaTemplateLibrary/Libs.hpp
@@ -26,16 +26,16 @@ namespace LTL
 
     namespace Libs
     {
-        const Lib global = { LUA_GNAME, luaopen_base };
-        const Lib package = { LUA_LOADLIBNAME, luaopen_package };
-        const Lib coroutine = { LUA_COLIBNAME, luaopen_coroutine };
-        const Lib table = { LUA_TABLIBNAME, luaopen_table };
-        const Lib io = { LUA_IOLIBNAME, luaopen_io };
-        const Lib os = { LUA_OSLIBNAME, luaopen_os };
-        const Lib string = { LUA_STRLIBNAME, luaopen_string };
-        const Lib math = { LUA_MATHLIBNAME, luaopen_math };
-        const Lib utf8 = { LUA_UTF8LIBNAME, luaopen_utf8 };
-        const Lib debug = { LUA_DBLIBNAME, luaopen_debug };
+        const Lib global{ LUA_GNAME, luaopen_base };
+        const Lib package{ LUA_LOADLIBNAME, luaopen_package };
+        const Lib coroutine{ LUA_COLIBNAME, luaopen_coroutine };
+        const Lib table{ LUA_TABLIBNAME, luaopen_table };
+        const Lib io{ LUA_IOLIBNAME, luaopen_io };
+        const Lib os{ LUA_OSLIBNAME, luaopen_os };
+        const Lib string{ LUA_STRLIBNAME, luaopen_string };
+        const Lib math{ LUA_MATHLIBNAME, luaopen_math };
+        const Lib utf8{ LUA_UTF8LIBNAME, luaopen_utf8 };
+        const Lib debug{ LUA_DBLIBNAME, luaopen_debug };
     }
 
 }
