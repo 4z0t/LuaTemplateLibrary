be6b695d5fbb0e18928b56b2cbfbd54ad4668e84
4z0t <gastec139@gmail.com>
2024-04-28 23:26:12 +0300

    mark as constexpr

--- Source/LuaTemplateLibrary/Libs.hpp
+++ Source/LuaTemplateLibrary/Libs.hpp
@@ -9,7 +9,7 @@ namespace LTL
     {
     public:
         Lib() = delete;
-        Lib(const char* const name, lua_CFunction const func) : name(name), func(func) {}
+        constexpr Lib(const char* const name, lua_CFunction const func) : name(name), func(func) {}
 
         inline void Register(lua_State* l) const
         {
@@ -26,16 +26,16 @@ namespace LTL
 
     namespace Libs
     {
-        const Lib global{ LUA_GNAME, luaopen_base };
-        const Lib package{ LUA_LOADLIBNAME, luaopen_package };
-        const Lib coroutine{ LUA_COLIBNAME, luaopen_coroutine };
-        const Lib table{ LUA_TABLIBNAME, luaopen_table };
-        const Lib io{ LUA_IOLIBNAME, luaopen_io };
-        const Lib os{ LUA_OSLIBNAME, luaopen_os };
-        const Lib string{ LUA_STRLIBNAME, luaopen_string };
-        const Lib math{ LUA_MATHLIBNAME, luaopen_math };
-        const Lib utf8{ LUA_UTF8LIBNAME, luaopen_utf8 };
-        const Lib debug{ LUA_DBLIBNAME, luaopen_debug };
+        constexpr Lib global{ LUA_GNAME, luaopen_base };
+        constexpr Lib package{ LUA_LOADLIBNAME, luaopen_package };
+        constexpr Lib coroutine{ LUA_COLIBNAME, luaopen_coroutine };
+        constexpr Lib table{ LUA_TABLIBNAME, luaopen_table };
+        constexpr Lib io{ LUA_IOLIBNAME, luaopen_io };
+        constexpr Lib os{ LUA_OSLIBNAME, luaopen_os };
+        constexpr Lib string{ LUA_STRLIBNAME, luaopen_string };
+        constexpr Lib math{ LUA_MATHLIBNAME, luaopen_math };
+        constexpr Lib utf8{ LUA_UTF8LIBNAME, luaopen_utf8 };
+        constexpr Lib debug{ LUA_DBLIBNAME, luaopen_debug };
     }
 
 }
