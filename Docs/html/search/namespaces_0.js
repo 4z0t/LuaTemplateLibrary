ca46e3e16c48f9e4cebd53dff56ab6462046580a
4z0t <gastec139@gmail.com>
2024-05-12 23:36:52 +0300

    update docs

--- Docs/html/search/namespaces_0.js
+++ Docs/html/search/namespaces_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['ltl_3a_3ametamethods_0',['MetaMethods',['../namespace_l_t_l_1_1_meta_methods.html',1,'LTL']]]
+  ['ltl_0',['LTL',['../namespace_l_t_l.html',1,'']]],
+  ['ltl_3a_3ametamethods_1',['MetaMethods',['../namespace_l_t_l_1_1_meta_methods.html',1,'LTL']]]
 ];
