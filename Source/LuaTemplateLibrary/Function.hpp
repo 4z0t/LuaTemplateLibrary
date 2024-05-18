b2c0fed26fe2fa428065aa3514ee0ad57212de77
4z0t <gastec139@gmail.com>
2024-05-18 23:33:49 +0300

    wip

--- Source/LuaTemplateLibrary/Function.hpp
+++ Source/LuaTemplateLibrary/Function.hpp
@@ -7,9 +7,7 @@ namespace LTL
 {
     namespace Internal
     {
-        struct CFunctionBase
-        {
-        };
+        struct CFunctionBase {};
 
         template <typename... TArgs>
         struct FunctionHelper : CFunctionBase
